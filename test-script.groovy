f6d4d21af4d7c722142d35c23c49c1d82aa9db7e
Aleksey Koch <mimicsmile@yandex.ru>
2019-07-30 16:44:35 +0300

    try to fix upload library

--- test-script.groovy
+++ test-script.groovy
@@ -1,4 +1,5 @@
-def lib = library (identifier: 'example@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git']))
+library identifier: 'tools', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git', traits: [[$class: 'jenkins.plugins.git.traits.BranchDiscoveryTrait']]])
+// def lib = library (identifier: 'example@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git']))
 
 String testUrl = "http://quickstart.cloudera:14000"
 String testDirname = "project-1-tmp"
@@ -6,7 +7,7 @@ String testUserName = "cloudera-scm"
 String testPath = "/home/"
 String testFile = "config.xml"
 
-t = lib.HadoopApiTools()
+def tools = new HadoopApiTools()
 
 
 node {
@@ -16,14 +17,14 @@ node {
 
     stage('Get status of directory in HDFS') {
 
-        def info1 = t.getStatusOfDir(testUrl, testUserName, testDirname)
+        def info1 = tools.getStatusOfDir(testUrl, testUserName, testDirname)
         println(info1)
 
     }
     
     stage('Put Files In Hdfs'){
 
-        def info2 = t.PutFilesInHdfs(testUrl,testUserName,testDirname,testPath,testFile)
+        def info2 = tools.PutFilesInHdfs(testUrl,testUserName,testDirname,testPath,testFile)
         println(info2)
     }
 

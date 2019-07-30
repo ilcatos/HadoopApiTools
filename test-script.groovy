fc54a074622cece2214de4c883ed2076a47549d7
Aleksey Koch <mimicsmile@yandex.ru>
2019-07-30 13:24:14 +0300

    try to fix upload library

--- test-script.groovy
+++ test-script.groovy
@@ -1,4 +1,4 @@
-def lib = library (identifier: 'HadoopApiTools@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git'])).api.tools.HadoopApiTools.new()
+def lib = library (identifier: 'example@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git'])).api.tools.HadoopApiTools.new()
 
 String testUrl = "http://quickstart.cloudera:14000"
 String testDirname = "project-1-tmp"

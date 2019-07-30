d03013cf4347e61569aa084c59995a2f1dd8fe5e
ilcatos <mimicsmile@yandex.ru>
2019-07-30 09:37:38 +0300

    add HadoopApiTools.groovy

---
+++ HadoopApiTools.groovy
@@ -0,0 +1,41 @@
+import groovy.json.JsonSlurper
+
+class HadoopApiTools {
+
+    static void getStatusOfDir(String httpfs_url, String hadoop_user, String hdfs_dirname) {
+
+        def process = ['bash', '-c', "curl --negotiate -u : \"${httpfs_url}/webhdfs/v1/user/${hadoop_user}/${hdfs_dirname}/?op=LISTSTATUS\""].execute()
+        process.waitFor()
+        def info = new JsonSlurper().parseText(process.text)
+
+        if (info.FileStatuses == null) {
+
+            return info.RemoteException
+
+        } else {
+
+            return info.FileStatuses
+
+        }
+
+    }
+
+
+    static void PutFilesInHdfs(String httpfs_url, String hadoop_user, String hdfs_dirname, String PathTofile, String file) {
+
+        def process = ['bash', '-c', "curl --negotiate -X PUT -L -u : \"${httpfs_url}/webhdfs/v1/user/${hadoop_user}/${hdfs_dirname}/${file}?op=CREATE&&user.name=${hadoop_user}\"  --header \'Content-Type: application/octet-stream\' -T ${PathTofile}\${file} "].execute()
+        process.waitFor()
+        def info = new JsonSlurper().parseText(process.text)
+
+        if (info.FileStatuses == null) {
+
+            return info.RemoteException.message
+
+        } else {
+
+            return info.FileStatuses
+
+        }
+
+    }
+}

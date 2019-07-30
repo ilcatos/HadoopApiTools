d024e9fd60095d87cabbfa236267908a9e4faeac
Aleksey Koch <mimicsmile@yandex.ru>
2019-07-30 13:40:26 +0300

    try to fix upload library

--- test-script.groovy
+++ test-script.groovy
@@ -1,4 +1,4 @@
-def lib = library (identifier: 'example@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git'])).api.tools.HadoopApiTools()
+def lib = library (identifier: 'example@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'dbd9bf1b-0fd7-45da-b7c8-a7c3c3e03a23', remote: 'https://github.com/ilcatos/HadoopApiTools.git'])).api.tools
 
 String testUrl = "http://quickstart.cloudera:14000"
 String testDirname = "project-1-tmp"
@@ -6,7 +6,7 @@ String testUserName = "cloudera-scm"
 String testPath = "/home/"
 String testFile = "config.xml"
 
-
+t = new lib.HadoopApiTools()
 
 
 node {
@@ -16,14 +16,14 @@ node {
 
     stage('Get status of directory in HDFS') {
 
-        def info1 = lib.HadoopApiTools.new().getStatusOfDir(testUrl, testUserName, testDirname)
+        def info1 = t.getStatusOfDir(testUrl, testUserName, testDirname)
         println(info1)
 
     }
     
     stage('Put Files In Hdfs'){
 
-        def info2 = lib.HadoopApiTools.new().PutFilesInHdfs(testUrl,testUserName,testDirname,testPath,testFile)
+        def info2 = t.PutFilesInHdfs(testUrl,testUserName,testDirname,testPath,testFile)
         println(info2)
     }
 

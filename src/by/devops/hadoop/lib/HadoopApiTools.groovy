75e1cf0c8ea7f223587a2abdbc2ec5756a10576f
Aleksey Koch <mimicsmile@yandex.ru>
2019-08-02 15:43:00 +0300

    edit directory path

--- src/by/devops/hadoop/lib/HadoopApiTools.groovy
+++ src/by/devops/hadoop/lib/HadoopApiTools.groovy
@@ -34,7 +34,7 @@ import groovy.json.JsonSlurper
  */
 
         def info = sh (
-                script:  "curl --negotiate -X PUT -L -u : '${httpfs_url}/webhdfs/v1/user/${hadoop_user}/${hdfs_dirname}/${file}?op=CREATE&&user.name=${hadoop_user}'  --header 'Content-Type: application/octet-stream' -T ${PathTofile}/${file}",
+                script:  "curl --negotiate -X PUT -L -u : \"${httpfs_url}/webhdfs/v1/user/${hadoop_user}/${hdfs_dirname}/${file}?op=CREATE&&user.name=${hadoop_user}\"  --header  \"Content-Type: application/octet-stream\" -T ${PathTofile}/${file}",
                 returnStdout: true).trim()
 
         def infoOutput = new JsonSlurper().parseText(info)
